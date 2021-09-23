7ee0f619686a156fbd7f0699f0e6f403186400a6
Bret DuBois <bretdubois1@gmail.com>
2021-09-23 11:52:19 -0700

    Initial Commit

---
+++ Shared/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Shared
+//
+//  Created by Bret DuBois on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
